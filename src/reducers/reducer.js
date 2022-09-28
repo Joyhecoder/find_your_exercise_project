c23ffcfea8d7073e4a91ee07e5a18c5d90c01a90
Joy He <joyhe520@gmail.com>
2022-09-28 17:36:09 -0500

    initial set up

---
+++ src/reducers/reducer.js
@@ -0,0 +1,11 @@
+const reducers = (state, action) => { 
+    if(state===undefined){
+        state ={
+            excercise:[]
+        }
+
+    }
+    return state;
+ }
+
+ export default reducers
